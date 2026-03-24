76ec78eed2687d9db080ec9b806674bdafa404c8
shreaks-00 <maxer.tiee@gmail.com>
2026-03-24 14:39:23 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Our "Database" of fruits
+fruits_db = [
+    {"id": 1, "name": "Apple", "color": "Red"},
+    {"id": 2, "name": "Banana", "color": "Yellow"},
+    {"id": 3, "name": "Blueberry", "color": "Blue"},
+    {"id": 4, "name": "Mango", "color": "Orange"}
+]
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to the Fruit API! Go to /fruits to see the list."}
+
+# Endpoint to get the whole list
+@app.get("/fruits")
+def get_all_fruits():
+    return fruits_db
+
+# Endpoint to get just one fruit by its ID
+@app.get("/fruits/{fruit_id}")
+def get_fruit(fruit_id: int):
+    for fruit in fruits_db:
+        if fruit["id"] == fruit_id:
+            return fruit
+    return {"error": "Fruit not found"}
